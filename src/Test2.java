ed97071843c64df1da238ce510825ba55fbae08b
rms <ramesh91kr@gmail.com>
2023-09-12 13:56:43 +0530

    first commit by Engineer.

---
+++ src/Test2.java
@@ -0,0 +1,12 @@
+
+public class Test2 {
+public static void main(String[] args) {
+	String name = "Suresh Kumar";
+	
+	if(name == "Suresh Kumar") {
+		System.out.println("Yes, this is me");
+	}
+	else
+		System.out.println("No, this is not me.");
+}
+}
