fc8ac4382823f922d4d66e0fbef82ad46f314e38
rms <ramesh91kr@gmail.com>
2023-09-12 15:03:12 +0530

    added additional checks

--- src/Test2.java
+++ src/Test2.java
@@ -3,7 +3,7 @@ public class Test2 {
 public static void main(String[] args) {
 	String name = "Suresh Kumar";
 	
-	if(name == "Suresh Kumar") {
+	if(name == "Suresh Kumar" || name=="suresh kumar") {
 		System.out.println("Yes, this is me");
 	}
 	else
